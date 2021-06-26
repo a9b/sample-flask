4df7edc8a50256d596f0adb0ee00e5b80745717c
a9b <a9b.word@gmail.com>
2021-06-26 20:45:04 +0900

    Revert "この修正をすると amqp://guest:**@127.0.0.1:5672// に繋ごうとする"
    This reverts commit e02b89912b271622b3c5c97e55aae21f354509c5.

--- testapp/__init__.py
+++ testapp/__init__.py
@@ -2,10 +2,10 @@ from flask import Flask
 from testapp.celery_maker import make_celery
 
 app = Flask(__name__)
-# app.config.update(
-#     CELERY_BROKER_URL='redis://localhost:6379',
-#     CELERY_RESULT_BACKEND='redis://localhost:6379'
-# )
+app.config.update(
+    CELERY_BROKER_URL='redis://localhost:6379',
+    CELERY_RESULT_BACKEND='redis://localhost:6379'
+)
 celery = make_celery(app)
 
 import testapp.route
